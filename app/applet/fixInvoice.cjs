b2dc96422763c89db5c4db1057d9828b1819c132
EslamSoud <eslamsoud33@gmail.com>
2026-06-01 01:01:28 +0300

    حذف نهائي

--- app/applet/fixInvoice.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-let c = fs.readFileSync('src/components/InvoiceTab.tsx', 'utf8');
-
-c = c.replace('        </div>\n\n      </div>\n\n      {/* Success Modal - Offers Download Receipt Image & WhatsApp Share */}\n          </>\n        )}', '          </>\n        )}');
-
-fs.writeFileSync('src/components/InvoiceTab.tsx', c);
